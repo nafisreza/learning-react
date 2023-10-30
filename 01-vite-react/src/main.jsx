71fdb66383ccfdc3b49ce562fb87386f0c9ba2cc
nafisreza <bigganinafis@gmail.com>
2023-10-30 19:53:01 +0600

    🔥 createElement to manually pass html element

--- 01-vite-react/src/main.jsx
+++ 01-vite-react/src/main.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = React.createElement(
+  'a',                                              // type
+  {href: 'https://google.com', target: '_blank'},   // attributes
+  'Click me to visit google'                        // value 
+
+)
+
 const anotherElement = (
   <a href="https://google.com" target='_blank'>Visit Google</a>
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-   anotherElement
+   reactElement
 )
