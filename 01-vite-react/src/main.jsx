4d99c6d3a72f4f3b3715c2398d6c6817baa00904
nafisreza <bigganinafis@gmail.com>
2023-10-30 19:51:51 +0600

    🎉 Creating manual DOM Tree for react

--- 01-vite-react/src/main.jsx
+++ 01-vite-react/src/main.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   anotherElement
 )
