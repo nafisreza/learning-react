b6c5c0767b770086381a748f50e61e61b5c82d52
nafisreza <bigganinafis@gmail.com>
2023-10-30 19:29:12 +0600

    🚀 Efficiently rendering components

--- custom-react/customreact.js
+++ custom-react/customreact.js
@@ -9,6 +9,8 @@ const reactElement = {
 
 const mainContainer = document.querySelector('#root')
 
+/* Manual way to render a component on the webpage
+
 function customRender(reactElement, container){
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.value
@@ -16,6 +18,21 @@ function customRender(reactElement, container){
     domElement.setAttribute('target', reactElement.props.target)
     container.appendChild(domElement)
     console.log('benchdod')
+}
+customRender(reactElement, mainContainer)
+
+*/
+
+// Efficient way to render components with DRY principle
+
+function customRender(reactElement, container){
+const domElement = document.createElement(reactElement.type)
+domElement.innerHTML = reactElement.value
+for (const prop in reactElement.props) {
+    if (prop === 'children') continue
+    domElement.setAttribute(prop, reactElement.props[prop])
+}
+container.appendChild(domElement)
 }
 
 customRender(reactElement, mainContainer)
